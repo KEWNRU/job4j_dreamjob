071e47df5878d345ea3511c06364890a22b88308
KEWNRU <n0kky21rus@gmail.com>
2024-10-22 17:28:04 +0300

    5. Просмотр, редактирование и удаление вакансии [#504850]

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -10,9 +10,6 @@ public class Vacancy {
     private String description;
     private LocalDateTime creationDate;
 
-    public Vacancy(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
 
     public Vacancy(int id, String title, String description, LocalDateTime creationDate) {
         this.id = id;
